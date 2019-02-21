50cb0f70c7643eee39433b6f712a9f3db69f94c6
Macbook Pro <macbookpro@Macbooks-MacBook-Pro.local>
2019-02-21 11:50:21 -0800

    start

---
+++ App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Gangsan Lee</Text>
+        <Text>Eric Choi</Text>
+        <Text>Welcome</Text>
+        <Text>123</Text>
+
+      </View>
+
+
+
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#358',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
